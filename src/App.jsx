f7a384930a80499ba6b3a32983e36007bf664d10
kmk9000 <kallekoivuniemi@gmail.com>
2025-10-30 12:44:21 +0200

    changed data from js to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import PersonCard from "./components/PersonCard";
-import employees from "./data";
 import PersonList from "./components/PersonList";
 
 function App() {
--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,7 +1,15 @@
 import PersonCard from "./PersonCard";
-import employees from "../data";
+import employeeData from "../assets/employeeData.json";
+// comment: former js data import
+// import employees from "../data";
+// export default function PersonList() {
+//   return employees.map((employee) => (
+//     <PersonCard key={employee.id} {...employee} />
+//   ));
+// }
+
 export default function PersonList() {
-  return employees.map((employee) => (
+  return employeeData.map((employee) => (
     <PersonCard key={employee.id} {...employee} />
   ));
 }
