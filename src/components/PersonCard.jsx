878cc95d5401107535789e7b1dad1a02ee7543d4
kmk9000 <kallekoivuniemi@gmail.com>
2025-11-03 13:00:05 +0200

    week3 start

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,6 +1,6 @@
 import animalEmojis from "../assets/animalEmojis.json";
 
-export default function Card({
+export default function PersonCard({
   name,
   title,
   salary,
@@ -17,6 +17,14 @@ export default function Card({
   return (
     <div className="card">
       <h2>{name}</h2>
+      <div style={{ color: "red" }}>
+        {yearsEmployed < 0.5 ? "🔔 Schedule probation review." : ""}
+      </div>
+      <div style={{ color: "green" }}>
+        {yearsEmployed == 5 ? "🎉 Schedule recognition meeting." : ""}
+        {yearsEmployed == 10 ? "🎉 Schedule recognition meeting." : ""}
+        {yearsEmployed == 15 ? "🎉 Schedule recognition meeting." : ""}
+      </div>
       <p>Title: {title}</p>
       <p>Salary: {salary}</p>
       <p>Phone: {phone}</p>
@@ -27,15 +35,6 @@ export default function Card({
       </p>
       <p>Start date: {startDate}</p>
       <p>Years employed: {yearsEmployed}</p>
-      <div style={{ color: "red" }}>
-        {yearsEmployed < 0.5 ? "🔔 Schedule probation review." : ""}
-      </div>
-      <div style={{ color: "green" }}>
-        {yearsEmployed == 5 ? "🎉 Schedule recognition meeting." : ""}
-        {yearsEmployed == 10 ? "🎉 Schedule recognition meeting." : ""}
-        {yearsEmployed == 15 ? "🎉 Schedule recognition meeting." : ""}
-      </div>
-
       <p>Location: {location}</p>
       <p>Department: {department}</p>
       <p>Skills: {skills.join(", ")}</p>
