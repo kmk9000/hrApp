94bb6dee4be5a733b1772423ddbdf00d2835d954
kmk9000 <kallekoivuniemi@gmail.com>
2025-11-07 14:49:13 +0200

    additional stylings

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ export default function About() {
   return (
     <div className="card">
       <h2>About hrApp</h2>
-      <p>This is a simple HR management application for tracking employees.</p>
+      <p>
+        This is a simple HR management application for tracking employees. What
+        about it, <i>huh</i>?
+      </p>
     </div>
   );
 }
