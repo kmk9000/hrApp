01137bf1cf3bb139262eccc1fce9c5974802ba74
kmk9000 <kallekoivuniemi@gmail.com>
2025-11-07 13:01:26 +0200

    add about page content

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="card">
+      <h2>About hrApp</h2>
+      <p>This is a simple HR management application for tracking employees.</p>
+    </div>
+  );
+}
